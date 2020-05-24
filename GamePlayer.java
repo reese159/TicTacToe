4cbe1a3d5a6f8e746d3f9d462aa8b86c6fb485a1
reese159 <42494851+reese159@users.noreply.github.com>
2020-05-23 20:17:28 -0400

    GUI Update
    Added GUI shell to be fully implemented in future build.

--- GamePlayer.java
+++ GamePlayer.java
@@ -1,15 +1,27 @@
 import java.util.Scanner; 
+import java.awt.event.*; 
+import java.awt.*; 
+import javax.swing.*;
 
 /**
  * Plays the tic-tac-toe game. eventually, the AI will never lose.
  * @author Reese Pelletier, Justin Briley
  *
  */
-public class GamePlayer {
-	
-	//================================FIELDS=====================================
-	static final Scanner scan = new Scanner(System.in);
+@SuppressWarnings("serial")
+public class GamePlayer extends JFrame{
 
+	//================================FIELDS=====================================
+	//Scanner set
+	static final Scanner scan = new Scanner(System.in);
+	//GUI setup
+	static JFrame frame;
+	static JButton l1, l2, l3, l4, l5, l6, l7, l8, l9;
+	static JLabel title;
+	static JPanel win;
+	
+	
+	
 	//================================HELPER METHODS=====================================
 	/**
 	 * 
@@ -161,81 +173,133 @@ public class GamePlayer {
 		}
 		return false;
 	}
-
 	
+	/**
+	 * Two player game
+	 */
 	public static void twoPlayerGame() {
 		//Show user how game works
-				gamePrelim();
-				int OWins = 0;
-				int XWins = 0;
-				int Cats = 0;
-				boolean again = true;
-				//Prompt User For Input & Set Input Values
-				while(again == true) {
-					int counter = 0;
-					boolean win = false;
-					int locArray[] = new int[9];
-					String gameBoard[] = initGameBoard();
-					while(win == false) {
-						int loc = valuePrompter();
-						locArray[0] = loc;
-						while(validityCheck(gameBoard, loc) == false) {
-							System.out.println("That move was invalid. Please try again.");
-							loc = valuePrompter();
-						}
-						//Update the game board with move
-						update(gameBoard, loc, counter);
-						win = winCheck(gameBoard, counter);
-						if(win == true) {
-							break;
-						}
-						counter++;
-						if(counter == 9) {
-							Cats++;
-							System.out.println("Tied");
-							System.out.println("X Wins: " + XWins);
-							System.out.println("O Wins: " + OWins);
-							System.out.println("Ties: " + Cats);	
-							break;
-						}
-					}
-					if(win == true) {
-						if(counter % 2 == 0) {
-							XWins++;
-							System.out.println("XvX");
-							System.out.println("X Wins: " + XWins);
-							System.out.println("O Wins: " + OWins);		
-							System.out.println("Ties: " + Cats);	
-						}
-						else {
-							OWins++;
-							System.out.println("OwO");
-							System.out.println("X Wins: " + XWins);
-							System.out.println("O Wins: " + OWins);
-							System.out.println("Ties: " + Cats);	
-						}
-					}
-					System.out.println("Would you like to play again?");
-					System.out.println("Y/N");
-					String decide = scan.next().toLowerCase();
-					if(decide.equals("n")) {
-						again = false;
-						System.out.println("Good Game.");
-						System.out.println("No Rematch.");
-						System.out.println("GGNORE");
-					}
-					else if(decide.equals("y")) {
-						again = true;
-						System.out.println("Onto the next game.");
-						System.out.println();
-					}
-					else {
-						again = false;
-						System.out.println("I'll take that as a no...");
-				
-					}
+		gamePrelim();
+		//Set Initial Variables
+		int OWins = 0;
+		int XWins = 0;
+		int Cats = 0;
+		boolean again = true;
+		//Game Loop
+		while(again == true) {
+			int counter = 0;
+			boolean win = false;
+			int locArray[] = new int[9];
+			String gameBoard[] = initGameBoard();
+			//Game play
+			while(win == false) {
+				int loc = valuePrompter();
+				locArray[0] = loc;
+				while(validityCheck(gameBoard, loc) == false) {
+					System.out.println("That move was invalid. Please try again.");
+					loc = valuePrompter();
 				}
+				//Update the game board with move
+				update(gameBoard, loc, counter);
+				win = winCheck(gameBoard, counter);
+				if(win == true) {
+					break;
+				}
+				counter++;
+				if(counter == 9) {
+					Cats++;
+					System.out.println("Tied");
+					System.out.println("X Wins: " + XWins);
+					System.out.println("O Wins: " + OWins);
+					System.out.println("Ties: " + Cats);	
+					break;
+				}
+			}
+			//Declare Winner
+			if(win == true) {
+				if(counter % 2 == 0) {
+					XWins++;
+					System.out.println("XvX");
+					System.out.println("X Wins: " + XWins);
+					System.out.println("O Wins: " + OWins);		
+					System.out.println("Ties: " + Cats);	
+				}
+				else {
+					OWins++;
+					System.out.println("OwO");
+					System.out.println("X Wins: " + XWins);
+					System.out.println("O Wins: " + OWins);
+					System.out.println("Ties: " + Cats);	
+				}
+			}
+			//Prompt for re match
+			System.out.println("Would you like to play again?");
+			System.out.println("Y/N");
+			String decide = scan.next().toLowerCase();
+			if(decide.equals("n")) {
+				again = false;
+				System.out.println("Good Game.");
+				System.out.println("No Rematch.");
+				System.out.println("GGNORE");
+			}
+			else if(decide.equals("y")) {
+				again = true;
+				System.out.println("Onto the next game.");
+				System.out.println();
+			}
+			else {
+				again = false;
+				System.out.println("I'll take that as a no...");
+		
+			}
+		}
 	}
+	
+	/**
+	 * Initialize Game Window
+	 */
+	static void twoPlayerGUIInit() {
+		//Create frame storing game
+		frame = new JFrame("Tic-Tac-Toe");
+		//Label game type
+		JLabel dummy = new JLabel("");
+		title = new JLabel("         Two-Player Game");
+		//Create button grid
+		l1 = new JButton("empty");
+		l2 = new JButton("empty");
+		l3 = new JButton("empty");
+		l4 = new JButton("empty");
+		l5 = new JButton("empty");
+		l6 = new JButton("empty");
+		l7 = new JButton("empty");
+		l8 = new JButton("empty");
+		l9 = new JButton("empty");
+		//Create game window
+		win = new JPanel();
+		win.setLayout(new GridLayout(0,3));
+		//Add assets & setup game window
+		win.add(l1);
+		win.add(l2);
+		win.add(l3);
+		win.add(l4);
+		win.add(l5);
+		win.add(l6);
+		win.add(l7);
+		win.add(l8);
+		win.add(l9);
+		win.add(dummy);
+		win.add(title);
+		Color background = new Color(32, 160, 230);
+		win.setBackground(background);
+		frame.add(win);
+		frame.setSize(500,450);
+		frame.setVisible(true);
+	}
+	
+	public static void twoPlayerGameGUI() {
+		twoPlayerGUIInit();
+	}
+	
 	//================================MAIN METHOD=====================================
 	/**
 	 * Runs the game
@@ -243,13 +307,25 @@ public class GamePlayer {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Would you like to play 1 player or 2 player?");
-		System.out.println("1/2");
+		System.out.println("1 or 2");
 		int gameType = scan.nextInt();
 		if(gameType == 1) {
 			System.out.println("Not Implemented");
 		}
 		else if(gameType == 2) {
-			twoPlayerGame();
+			System.out.println("Hacker Mode?");
+			System.out.println("Y/N");
+			String decide = scan.next().toLowerCase();
+			if(decide.equals("y")) {
+				twoPlayerGame();
+			}
+			else if(decide.equals("n")){
+				twoPlayerGUIInit();
+			}
+			else {
+				System.out.println("Not smart enough for hacker mode...");
+				twoPlayerGUIInit();
+			}
 		}
 		else {
 			System.out.println("No");
